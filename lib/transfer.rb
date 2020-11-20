5248f5069e32700b927653d14fb78a3eca45e792
Zach <zbayard@gmail.com>
2020-11-20 10:34:01 -0500

    commit tree 3b1cef0b9ebd1d6baf87456c1718ff00cf0eb1ef

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,19 @@
+require 'pry'
 class Transfer
   # your code here
+  attr_accessor :sender, :receiver, :status, :amount, :balance
+
+  def initialize(sender, receiver, status = "pending", amount)
+      @sender = sender 
+      @receiver = receiver
+      @status = status
+      @amount = amount
+      binding.pry 
+  end 
+  
+
+  def valid?
+    # BankAccount.valid?
+  
+    #Transfer #valid? can check that both accounts are valid aka sender & receiver 
 end
